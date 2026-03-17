e804fc4229c59fe337cb40725ce61a99ca2e51e1
VilyVargas <vily05dominic23@gmail.com>
2026-03-17 10:16:58 -0600

    Avance 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 import App from './App.jsx'
 
@@ -8,3 +9,4 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
